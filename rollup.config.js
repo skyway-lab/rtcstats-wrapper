c3dddb28ceb7a0163f57f870d45c0467e65104be
matsub <matsub.rk@gmail.com>
2019-07-08 08:58:31 +0900

    Update build config and upload build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,17 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import builtins from "rollup-plugin-node-builtins";
+
 export default {
   input: "./src/main.js",
   output: {
     file: "./dist/rtcstats-wrapper.js",
-    format: "umd"
-  }
+    format: "umd",
+    name: "RTCStatsMoment"
+  },
+  plugins: [
+    resolve({ preferBuiltins: true, mainFields: ["browser"] }),
+    commonjs(),
+    builtins()
+  ]
 };
